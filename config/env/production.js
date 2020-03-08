f1acbc9766eada78429bcca6215ac1705bfcf845
pedro cuasqui <pedro.cuasqui@gmail.com>
2020-03-08 12:24:34 -0500

    corrige rutas en production.js

--- config/env/production.js
+++ config/env/production.js
@@ -248,7 +248,7 @@ module.exports = {
 		 ***************************************************************************/
 		onlyAllowOrigins: [
 			"https://git.heroku.com",
-			"https://vast-wildwood-67076.herokuapp.com"
+			"https://obscure-harbor-71757.herokuapp.com"
 		]
 		/***************************************************************************
 		 *                                                                          *
@@ -352,7 +352,7 @@ module.exports = {
 	 *                                                                         *
 	 ***************************************************************************/
 	custom: {
-		baseUrl: "https://vast-wildwood-67076.herokuapp.com/",
+		baseUrl: "https://obscure-harbor-71757.herokuapp.com",
 		imageBaseUrl: "https://obscure-harbor-71757.herokuapp.com/images/uploaded",
 
 		rememberMeCookieMaxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
